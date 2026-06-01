dcff888bcd427b31ba5d2a7dc13ce99cef4bc358
alexey_nik <argonikulin@gmail.com>
2026-06-01 20:37:51 +0300

    log not

--- task_1/build.gradle.kts
+++ task_1/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+    runtimeOnly("org.slf4j:slf4j-nop:2.0.17")
 
     testImplementation(kotlin("test"))
 }
